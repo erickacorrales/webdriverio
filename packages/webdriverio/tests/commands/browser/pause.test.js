128ff596ef9d0112ce9cecf1ef0c974f73feee9e
christian-bromann <mail@christian-bromann.com>
2019-06-20 18:02:18 +0200

    more unit tests

--- packages/webdriverio/tests/commands/browser/pause.test.js
+++ packages/webdriverio/tests/commands/browser/pause.test.js
@@ -1,8 +1,8 @@
 import request from 'request'
 import { remote } from '../../../src'
 
-describe('isEnabled test', () => {
-    it('should allow to check if an element is enabled', async () => {
+describe('pause test', () => {
+    it('should pause with provided timeout', async () => {
         const browser = await remote({
             baseUrl: 'http://foobar.com',
             capabilities: {
@@ -11,8 +11,26 @@ describe('isEnabled test', () => {
         })
 
         const start = Date.now()
-        await browser.pause(1000)
+        await browser.pause(500)
+        expect((Date.now() - start) > 490).toBe(true)
+        expect(request.mock.calls).toHaveLength(1)
+    })
+
+    it('should pause by 1s per default', async () => {
+        const browser = await remote({
+            baseUrl: 'http://foobar.com',
+            capabilities: {
+                browserName: 'foobar'
+            }
+        })
+
+        const start = Date.now()
+        await browser.pause()
         expect((Date.now() - start) > 990).toBe(true)
         expect(request.mock.calls).toHaveLength(1)
+    })
+
+    afterEach (() => {
+        request.mockClear()
     })
 })
